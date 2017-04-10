9a879e7cc952a220045d4dadc14aeea5eda5f7cc
Charuwit Nodthaisong <coladevteam@gmail.com>
2017-04-11 01:57:41 +0700

    add label and label style to slider bar

--- src/app/slider-bar/slider-bar.component.ts
+++ src/app/slider-bar/slider-bar.component.ts
@@ -1,18 +1,50 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-slider-bar',
   templateUrl: './slider-bar.component.html',
   styleUrls: ['./slider-bar.component.scss']
 })
-export class SliderBarComponent implements OnInit {
+export class SliderBarComponent implements OnInit, OnChanges {
 
 	@Input() min: number = 0;
 	@Input() max: number = 100;
+	@Input() opts: any;
+
+	labels: Array<any> = [];
+	style: any = {
+		label: {
+			"width": "100%" 
+		}
+	}
 	
   constructor() { }
 
   ngOnInit() {
+  	/* Compute Label */
+
+  	this.labels = this.getLabels();
+  	console.log(this.labels)
+  }
+
+  ngOnChanges() {
+
+  }
+
+  getLabels() {
+  	const range = this.opts.label.range;
+  	let list = _.range(this.min, this.max, range);
+  	list.push(this.max)
+
+  	/* Set Label Style */
+  	this.style.label.width = (100 + (list.length - 1)) + "%"
+  	return list.map((item: any) => {
+  		return {
+  			label: item,
+  			value: item
+  		}
+  	})
   }
 
 }
