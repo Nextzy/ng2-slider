858d6f11c6e017f9c2044630f26d4bdb25caf908
Charuwit Nodthaisong <coladevteam@gmail.com>
2017-04-14 16:09:29 +0700

    add popover style

--- src/app/slider-bar/slider-bar.component.ts
+++ src/app/slider-bar/slider-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnChanges, Input, ViewChild } from '@angular/core';
 import { SliderBarDirective } from './slider-bar.directive'
 import * as _ from 'underscore';
+import Utils from '../utils';
 
 @Component({
 	selector: 'app-slider-bar',
@@ -41,37 +42,15 @@ export class SliderBarComponent implements OnInit, OnChanges {
 
 	getLabels() {
 		const range = this.opts.label.range;
-		const format = this.getDecimalFormat(this.opts.label.format);
+		const format = Utils.getDecimalFormatter(this.opts.label.format);
 		let list = _.range(this.min, this.max, range);
 
 		/* Set Label Style */
 		return list.map((item: any) => {
 			return {
-				label: this.getLabelFormat(item, format),
+				label: Utils.formatValue(item, format),
 				value: item
 			}
 		})
-	}
-
-	getLabelFormat(value, format) {
-		const formatValue = value.toFixed(format.fixed)
-		return format.str.replace("{{number}}", formatValue)
-	}
-
-	getDecimalFormat(format) {
-		const hasDecimal = format.match(/\d+\.\d+/g)
-		if (hasDecimal) {
-			const fixed = (hasDecimal[0].split(".")[1] || "").length
-			return {
-				str: format.replace(hasDecimal[0], "{{number}}"),
-				fixed: fixed
-			}
-		}
-
-		const hasNumber = format.match(/\d+/g)
-		return {
-			str: format.replace(hasNumber, "{{number}}"),
-			fixed: 0
-		}
 	}
 }
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -15,4 +15,27 @@ export default class Utils {
 		
         return value
     }
+
+    static getDecimalFormatter(format) {
+
+		const hasDecimal = format.match(/\d+\.\d+/g)
+		if (hasDecimal) {
+			const fixed = (hasDecimal[0].split(".")[1] || "").length
+			return {
+				str: format.replace(hasDecimal[0], "{{number}}"),
+				fixed: fixed
+			}
+		}
+
+		const hasNumber = format.match(/\d+/g)
+		return {
+			str: format.replace(hasNumber, "{{number}}"),
+			fixed: 0
+		}
+    }
+
+    static formatValue(value, format) {
+		const formatValue = value.toFixed(format.fixed)
+		return format.str.replace("{{number}}", formatValue)
+    }
 }
